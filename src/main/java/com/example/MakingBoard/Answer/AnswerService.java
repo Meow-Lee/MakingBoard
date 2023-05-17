cc001c5e1031bd9af37464319bde7cd2d1e72ff7
DESKTOP-IFKVEA0\LeeMyungMo <lukei321@kakao.com>
2023-05-17 11:48:03 +0900

    Second Commit

---
+++ src/main/java/com/example/MakingBoard/Answer/AnswerService.java
@@ -0,0 +1,22 @@
+package com.example.MakingBoard.Answer;
+
+import com.example.MakingBoard.Question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content){
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
